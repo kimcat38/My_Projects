69d4e5371eeb259b28a187c1ff10c8bac4773b55
kimcat38 <kimyap39@gmail.com>
2023-01-21 00:49:47 +0800

    migrated the categories map context to redux

--- React_Projects/clothing-website/src/routes/category/category.component.jsx
+++ React_Projects/clothing-website/src/routes/category/category.component.jsx
@@ -1,7 +1,8 @@
-import { useContext, useState, useEffect, Fragment } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import { CategoriesContext } from "../../contexts/categories.context";
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
 import ProductCard from "../../component/product-card/product-card.component";
 
@@ -9,8 +10,10 @@ import { CategoryContainer, CategoryTitle } from "./category.styles.jsx";
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState([]);
+
+  const categoriesMap = useSelector(selectCategoriesMap);
+
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
