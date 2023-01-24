f53b421a2616fb6dbb7bfa7d9e6ab9c35e3f9f9f
kimcat38 <kimyap39@gmail.com>
2023-01-24 17:29:18 +0800

    adding redux thunk and loading icon

--- React_Projects/clothing-website/src/routes/category/category.component.jsx
+++ React_Projects/clothing-website/src/routes/category/category.component.jsx
@@ -2,8 +2,12 @@ import { useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/category.selector";
 
+import Spinner from "../../component/spinner/spinner.component";
 import ProductCard from "../../component/product-card/product-card.component";
 
 import { CategoryContainer, CategoryTitle } from "./category.styles.jsx";
@@ -11,6 +15,7 @@ import { CategoryContainer, CategoryTitle } from "./category.styles.jsx";
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -20,12 +25,16 @@ const Category = () => {
   return (
     <Fragment>
       <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </Fragment>
   );
 };
